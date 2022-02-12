af351e1ad3d5de7173916acaba4828f93fa24077
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2022-02-12 20:18:59 +0600

    modal functionality and horizontal card

--- components/Modal/modal-app.js
+++ components/Modal/modal-app.js
@@ -1,14 +1,16 @@
-const btnClose = document.querySelector(".modal-close");
-const modalWindow = document.querySelector(".modal");
+const btnClose = document.querySelector(".modal-style-close-btn");
+const modalWindow = document.querySelector(".modal-style-content");
 const modalBtn = document.querySelector("#modal-btn");
+const modalContainer = document.querySelector(".modal-style ");
 
 modalBtn.addEventListener("click", () => {
-    modalBtn.style.display = "none";
-    modalWindow.style.display = "block";
+  modalBtn.style.display = "none";
+  modalContainer.style.display = "block";
+  modalWindow.style.display = "block";
 });
 
-
 btnClose.addEventListener("click", () => {
-    modalWindow.style.display = "none";
-    modalBtn.style.display = "block";
+  modalWindow.style.display = "none";
+  modalBtn.style.display = "block";
+  modalContainer.style.display = "none";
 });
